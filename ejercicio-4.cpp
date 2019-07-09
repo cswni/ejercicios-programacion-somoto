a95cb4716ea899e29daa4015b3c0fc73420a4f44
CPEREZ <descarlos2013@gmail.com>
2019-07-08 21:15:19 -0600

    Ejercicio 4 - Cambiado a stdio lib

--- ejercicio-4.cpp
+++ ejercicio-4.cpp
@@ -2,23 +2,20 @@
 Hacer un programa en donde el usuario introduzca un numero y nos de como resultado
 la suma de los multiplos de 4 que hay entre el 1 hasta ese numero. POr ejemplo si el 
 usuario escribe 10 la suma de los multiplos de 4 es 12.
-
 */
 /*Agregamos iostream para usar el namespace std*/
-#include "iostream"
-/*Usar el namespace */
-using namespace std;
+#include <stdio.h>
 main (){
-	cout << "\n\tHacer un programa en donde el usuario introduzca un numero y nos de como resultado"<<endl;
-	cout << "\n\tla suma de los multiplos de 4 que hay entre el 1 hasta ese numero. POr ejemplo si el"<<endl; 
-	cout << "\n\tusuario escribe 10 la suma de los multiplos de 4 es 12"<<endl;
+	printf ( "\n\tHacer un programa en donde el usuario introduzca un numero y nos de como resultado");
+	printf ( "\n\tla suma de los multiplos de 4 que hay entre el 1 hasta ese numero. POr ejemplo si el"); 
+	printf ( "\n\tusuario escribe 10 la suma de los multiplos de 4 es 12");
 	/*Declaramos las variables que guarda ambos numeros y los impares*/
 	int numero, inicio =1, suma = 0;	
 	/*Solicitar el valor*/
-	cout << "\n\t Ingrese el numero maximo:";
-	cout << "\n\t:";
+	printf ("\n\t Ingrese el numero maximo:");
+	printf ("\n\t:");
 	/*Guardar el valor*/
-	cin >> numero;
+	scanf("%d",&numero);
 	/*Comprobar los valores para iniciar siempre con el menor*/
 	if(numero > inicio){
 		while(inicio <= numero){
@@ -27,7 +24,7 @@ main (){
 		}
 		inicio++;
 		}
-	cout << "\n\t La suma de los multiplos de 4 entre 1 y "<<numero<<" es = "<<suma<<endl;
+	printf ( "\n\t La suma de los multiplos de 4 entre 1 y %d es = %d",numero,suma);
 	}
 }
 
