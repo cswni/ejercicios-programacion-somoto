5d628d3776b7ae8f799a1bef502b8967952f1b45
CPEREZ <descarlos2013@gmail.com>
2019-07-08 21:21:11 -0600

    Ejercicio 5 - Cambiado a stdio lib

--- ejercicio-5.cpp
+++ ejercicio-5.cpp
@@ -3,53 +3,50 @@ Hacer un programa que el usuario elija cuantas notas ingresar y que imprima
 el tipo de aprendizaje obtenido por el estudiante.
 */
 /*Agregamos iostream para usar el namespace std*/
-#include "iostream"
-/*Usar el namespace */
-using namespace std;
+#include <stdio.h>
 main (){
-	cout << "\n\tHacer un programa que el usuario elija cuantas notas ingresar y que imprima"<<endl;
-	cout << "\n\tel tipo de aprendizaje obtenido por el estudiante"<<endl; 
-	cout << "\n\t***************************"<<endl;
-	cout << "\n\tAprendizaje avanzado >=90 "<<endl;
-	cout << "\n\tAprendizaje Satisfactorio >=76 y < 90 "<<endl;
-	cout << "\n\tAprendizaje basico >=60 y <=75 "<<endl;
-	cout << "\n\tApredizaje no alcanzado < 60 "<<endl;
-	cout << "\n\t**************************"<<endl;	
+	printf ( "\n\tHacer un programa que el usuario elija cuantas notas ingresar y que imprima");
+	printf ( "\n\tel tipo de aprendizaje obtenido por el estudiante"); 
+	printf ( "\n\t***************************");
+	printf ( "\n\tAprendizaje avanzado >=90 ");
+	printf ( "\n\tAprendizaje Satisfactorio >=76 y < 90 ");
+	printf ( "\n\tAprendizaje basico >=60 y <=75 ");
+	printf ( "\n\tApredizaje no alcanzado < 60 ");
+	printf ( "\n\t**************************");	
 	/*Declaramos las variables que guarda ambos numeros y los impares*/
 	int notas[20], numero, suma=0, promedio;	
 	/*Solicitar el valor*/
-	cout << "\n\t Cuantas notas desea ingresar:";
-	cout << "\n\t:";
+	printf ( "\n\t Cuantas notas desea ingresar:");
+	printf ( "\n\t:");
 	/*Guardar el valor*/
-	cin >> numero;
+	scanf("%d", &numero);
 	/*Comprobar los valores para iniciar siempre con el menor*/
 	if(numero <= 20){
 		int i = 1;
 		while(i <= numero){
-			cout << "\n\t Nota "<<i<<endl;
-			cout << "\n\t";
-			cin >>notas[i];
+			printf ( "\n\t Nota %d",i);
+			printf ( "\n\t");
+			scanf("%d",&notas[i]);
 			i++;
 		}
 		i = 1;
-		cout << "\n\t Total "<<numero<<endl;
+		printf ( "\n\t Total %d",numero);
 		while(i <= numero){
 			suma = suma + notas[i];
 			i++;
 		}
 		promedio = suma / numero;
 		/*Imprimir promedio*/
-		cout << "\n\t Promedio "<<promedio<<endl;
+		printf ( "\n\t Promedio : %d",promedio);
 		/*Determinar el aprendizaje*/
 		if(promedio >= 90){
-			cout << "\n\tAprendizaje avanzado"<<endl;
+			printf ("\n\tAprendizaje avanzado");
 		}else if(promedio >=76 && promedio <=89){
-			cout << "\n\tAprendizaje satisfactorio"<<endl;
+			printf ( "\n\tAprendizaje satisfactorio");
 		}else if(promedio >=60 && promedio <=75){
-			cout << "\n\tAprendizaje basico"<<endl;
-		}
-		else{
-			cout << "\n\tAprendizaje no alcanzado"<<endl;
+			printf ( "\n\tAprendizaje basico");
+		}else{
+			printf ( "\n\tAprendizaje no alcanzado");
 		}
 	}
 }
