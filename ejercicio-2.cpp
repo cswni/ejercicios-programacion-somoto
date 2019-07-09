f7c313570f96b793c04d104e63af4a4e995ec5d6
CPEREZ <descarlos2013@gmail.com>
2019-07-08 21:06:33 -0600

    Ejercicio 2 - Cambiado a stdio lib

--- ejercicio-2.cpp
+++ ejercicio-2.cpp
@@ -2,27 +2,25 @@
 Hacer un programa que sume los numeros pares del 1 hasta donde elija el usuario (ejemplo del 1 al 20 o del 1 al 40).
 */
 /*Agregamos iostream para usar el namespace std*/
-#include "iostream"
-/*Usar el namespace */
-using namespace std;
+#include <stdio.h>
 main (){
-	cout << "\n\tHacer un programa que sume los numeros pares del 1 hasta donde elija el usuario\n\t(ejemplo del 1 al 20 o del 1 al 40"<<endl;
+	printf("\n\tHacer un programa que sume los numeros pares del 1 hasta donde elija el usuario\n\t(ejemplo del 1 al 20 o del 1 al 40");
 	/*Declaramos la variable que guarda el numero ingresa y la suma*/
 	int numero = 0,suma =0;	
 	/*Solicitar el valor*/
-	cout << "\n\t Ingrese un numero:";
-	cout << "\n\t:";
+	printf("\n\t Ingrese un numero:");
+	printf ("\n\t:");
 	/*Guardar el valor*/
-	cin >> numero;
+	scanf ("%d",&numero);
 	/*Iniciar variable contadora a 1*/
 	int i = 1;
 	while(i <=numero){
 		if(i%2==0){
-			cout <<"\n\t "<< suma <<" + "<<i<<" = "<<suma + i<<endl;
+			printf ("\n\t %d + %d = %d",suma, i, suma + i);
 			suma = suma + i;
 		}
 		i++;
 	}
-	cout << "\n\t La suma total es :"<<suma<<endl;
+	printf ("\n\t La suma total es : %d",suma);
 }
 
